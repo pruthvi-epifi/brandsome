68ab4b114f992528a44a232bb64265bfb62b7295
Sandipan Mitra <sandipan.mitra001@gmail.com>
2021-05-24 02:14:27 +0530

    log view commented

--- routes/log.js
+++ routes/log.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Log = require('../models/Log');
 
-
+/*
 
 // GET ALL
 router.get('/', async (req,res) => {
@@ -16,5 +16,32 @@ router.get('/', async (req,res) => {
 
 });
 
+// GET HITS
+router.get('/hits', async (req,res) => {
+
+  try {
+    const data = await Log.find({ status: 'hit' })
+                          .populate('res')
+                          .select('req res -_id');
+    res.json(data);
+  } catch (err) {
+    res.json({ message: err });
+  }
+
+});
+
+// GET MISSES
+router.get('/misses', async (req,res) => {
+
+  try {
+    const data = await Log.find({ status: 'miss' }).select('req -_id');
+    res.json(data);
+  } catch (err) {
+    res.json({ message: err });
+  }
+
+});
+
+*/
 
 module.exports = router;
